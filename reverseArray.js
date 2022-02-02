e9973e91f774f4b8d7696e1ad83ef2614070c085
Debora Santos <49887714+debora-santos@users.noreply.github.com>
2022-02-02 05:16:30 -0300

    reversed array
    Input: [1,2,3]
    Output: [3,2,1]

---
+++ reverseArray.js
@@ -0,0 +1,32 @@
+function reverseArray (array) {
+  
+   // variable to insert the new array
+   let reversedArray = new Array;
+
+   // index is size array - 1, it will return the last index number
+   // ex: array=[1,2,3]; 
+        // array.length = 3; 
+        // 3-1 = 2 
+        // will return 2, the number of the last index: array[2] = 3
+    // it will start with the last index (biggest) to first (smallest)
+        // need to be decreasing
+   for (let i = array.length - 1; i >= 0; i--) {
+       
+       reversedArray.push(array[i]);
+       
+   }
+   
+   console.log('Reverse array using "for": '+ reversedArray);
+   
+    // solution using "while"
+    let newArray = [];
+    let i = array.length - 1;
+    
+    while (i >= 0) {
+        newArray.push(array[i]);
+        i--; 
+    }
+    console.log('Reverse array using "while": '+ newArray)
+}
+
+reverseArray([55, 1, 2, 3, 7, 10, 8]);
