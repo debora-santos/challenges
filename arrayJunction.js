6165fc390f8eb4999c2008dab8acd33502156961
Debora Santos <49887714+debora-santos@users.noreply.github.com>
2022-02-05 03:22:55 -0300

    add other way to solve arrayJunction

--- arrayJunction.js
+++ arrayJunction.js
@@ -17,3 +17,29 @@ function arrayJunction (arrays) {
 
 arrayJunction([[1,2,3],[4,5,6,7],[3,2,1]]);
 // output: [ 1, 2, 3, 4, 5, 6, 7, 3, 2, 1 ]
+
+
+// *-*-* Other way to solve without .concat() *-*-*
+
+function arraysJunction (arrays) {
+
+    let arrayUnion = new Array;
+    
+    for (let i = 0; i < arrays.length; i++) {
+        // access arrays    
+        let array = arrays[i];
+  
+        // access elements inside the array
+        for (let j = 0; j < array.length; j++) { 
+
+            arrayUnion.push(array[j]);
+
+        }
+    }
+    
+    console.log(arrayUnion);
+    
+}
+
+arraysJunction([[1,2,3,6],[4,5,6],[7,8,9],[10,11,12,22]]);
+// output: [ 1, 2, 3, 6, 4, 5, 6, 7, 8, 9, 10, 11, 12, 22 ]
