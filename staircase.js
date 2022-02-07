cbb3ba7375a2b1269ab096695c3c2d1500409c63
Debora Santos <49887714+debora-santos@users.noreply.github.com>
2022-02-07 01:01:46 -0300

    logic to create a staircase

---
+++ staircase.js
@@ -0,0 +1,13 @@
+function staircase (n) {
+  for (let i = 1; i <= n; i++) { 
+    console.log('-'.repeat(n-i) + '#'.repeat(i));
+  }
+}
+
+staircase(5);
+// output:
+// ----#
+// ---##
+// --###
+// -####
+// #####
